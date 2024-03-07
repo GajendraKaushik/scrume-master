3138cdedb4d7b68a7f62fb25850d64994fbcb9d6
GajendraKaushik <gajendrakaushik128@gmail.com>
2024-03-07 18:14:02 +0530

    project sidebare component added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import ProjectSideBare from './components/ProjectSideBar'
+
+function App() {
+  return (
+    <main className="h-screen my-8">
+    <ProjectSideBare />
+    </main>
+  )
+}
+
+export default App
